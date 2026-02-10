2642c3346d61eccce6002e852d6e9034d253d43c
Dragos Chisca <dragoschisca@gmail.com>
2026-02-10 22:09:07 +0200

    CREATE | ADD: SavedJob  propreties

--- backend/EasyApply.Core/Entites/SavedJob.cs
+++ backend/EasyApply.Core/Entites/SavedJob.cs
@@ -1,6 +1,13 @@
 namespace EasyApply.Core.Entites;
 
-public class SavedJobs
+public class SavedJob
 {
-    
+    public Guid Id { get; set; }
+    public Guid CandidateId { get; set; }
+    public Guid JobId { get; set; }
+    public DateTime SavedAt { get; set; } = DateTime.UtcNow;
+
+    // Navigation
+    public Candidate Candidate { get; set; } = null!;
+    public Job Job { get; set; } = null!;
 }
