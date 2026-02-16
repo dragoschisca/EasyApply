d45d766f11f99e861f0d811fc51cf02f3dff85c9
Dragos Chisca <dragoschisca@gmail.com>
2026-02-16 17:23:34 +0200

    ADD: Added JobController ApplicationDbContext

--- backend/EasyApplyAPI/Controllers/JobController.cs
+++ backend/EasyApplyAPI/Controllers/JobController.cs
@@ -1,6 +1,14 @@
+using EasyApplyAPI.Data;
+
 namespace EasyApplyAPI.Controllers;
 
 public class JobController
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public JobController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
 }
