e9a220052d5b58993a6407fb983f4788f09c6506
Ese Emir <tgtft1@gmail.com>
2026-02-21 00:49:33 +0300

    Am creat structura dto pentru Register

--- backend/EasyApplyAPI/DTOs/Auth/RegisterRequestDto.cs
+++ backend/EasyApplyAPI/DTOs/Auth/RegisterRequestDto.cs
@@ -2,5 +2,15 @@
 
 public class RegisterRequestDto
 {
-    
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+    public UserType UserType { get; set; }
+
+    // Candidate fields
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    // Company fields
+    public string? CompanyName { get; set; }
 }
