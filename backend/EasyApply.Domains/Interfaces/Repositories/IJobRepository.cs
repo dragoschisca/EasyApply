d2b93935d16a08b3ac81900d1ba6f91d139dfc4e
Ese Emir <tgtft1@gmail.com>
2026-03-02 18:35:51 +0300

    CREATE : JobRepo Interface

---
+++ backend/EasyApply.Domains/Interfaces/Repositories/IJobRepository.cs
@@ -0,0 +1,12 @@
+﻿using EasyApply.Core.Entites;
+
+namespace EasyApply.Domains.Interfaces.Repositories;
+
+public interface IJobRepository : EasyApply.Domains.Interfaces.Repositories.IBaseRepository<Job>
+{
+    Task<IEnumerable<Job>> GetByCompanyIdAsync(Guid companyId, bool activeOnly = true);
+    Task<(IEnumerable<Job> Jobs, int Total)> SearchAsync(
+        string? keyword, string? location, string? employmentType,
+        string? experienceLevel, bool? isRemote, int page, int pageSize);
+    Task IncrementViewCountAsync(Guid jobId);
+}
