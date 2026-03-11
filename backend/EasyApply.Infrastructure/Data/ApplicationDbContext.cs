9232a9bc3802758055308bd95247863c59ca525c
Dragos Chisca <dragoschisca@gmail.com>
2026-03-11 14:20:37 +0200

    ADD: DbSet for every entity

--- backend/EasyApply.Infrastructure/Data/ApplicationDbContext.cs
+++ backend/EasyApply.Infrastructure/Data/ApplicationDbContext.cs
@@ -11,4 +11,10 @@ public class ApplicationDbContext: DbContext
     }
     
     public DbSet<User> Users { get; set; }
+    public DbSet<Candidate> Candidates { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<CV> CVs { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<SavedJob> SavedJobs { get; set; }
 }
