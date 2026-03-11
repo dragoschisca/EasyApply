0ba5b034fce064deba6a1619f06a6d94c7450556
Dragos Chisca <dragoschisca@gmail.com>
2026-03-11 14:30:20 +0200

    REMOVE: Deactivate GetByRefreshTokenAsync

--- backend/EasyApply.Domains/Interfaces/Repositories/IUserRepository.cs
+++ backend/EasyApply.Domains/Interfaces/Repositories/IUserRepository.cs
@@ -6,5 +6,5 @@ namespace EasyApply.Domains.Interfaces.Repositories;
 public interface IUserRepository : IBaseRepository<User>
 {
     Task<User?> GetByEmailAsync(string email);
-    Task<User?> GetByRefreshTokenAsync(string refreshToken);
+   // Task<User?> GetByRefreshTokenAsync(string refreshToken);
 }
