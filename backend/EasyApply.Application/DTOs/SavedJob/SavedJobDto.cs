81df8122317a85f182c36ae66debba66a39fa07f
Dragos Chisca <dragoschisca@gmail.com>
2026-03-19 22:27:35 +0200

    CREATE: SavedJobDto for web controller

---
+++ backend/EasyApply.Application/DTOs/SavedJob/SavedJobDto.cs
@@ -0,0 +1,9 @@
+namespace EasyApply.Application.DTOs.SavedJob;
+
+public class SavedJobDto
+{
+    public Guid Id { get; set; }
+    public Guid CandidateId { get; set; }
+    public Guid JobId { get; set; }
+    public DateTime SavedAt { get; set; }
+}
