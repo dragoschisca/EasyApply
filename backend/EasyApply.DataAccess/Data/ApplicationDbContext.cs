b65bfbf206f73e93b6ab8a250680c7910a6fc8ab
Dragos Chisca <dragoschisca@gmail.com>
2026-04-05 00:15:35 +0300

    ADD: ApplicationDbContext and initial ApplicationDbContextModelSnapshot for data access and migrations setup

---
+++ backend/EasyApply.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using EasyApply.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyApply.DataAccess.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Candidate> Candidates { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<CV> CVs { get; set; }
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<SavedJob> SavedJobs { get; set; }
+}
