53c5eaf4d425f3841d6be1ecac124070423f643a
Bulgaru Valeriu <valeri.bulgaru06@gmail.com>
2026-05-18 15:04:37 +0300

    feat: add custom ConflictException and ForbiddenException classes

--- backend/EasyApply.BusinessLayer/Structure/Exceptions/Exception.cs
+++ backend/EasyApply.BusinessLayer/Structure/Exceptions/Exception.cs
@@ -24,3 +24,13 @@ public class BusinessException : Exception
 {
     public BusinessException(string message) : base(message) { }
 }
+
+public class ConflictException : Exception
+{
+    public ConflictException(string message) : base(message) { }
+}
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException(string message = "Access to this resource is forbidden.") : base(message) { }
+}
