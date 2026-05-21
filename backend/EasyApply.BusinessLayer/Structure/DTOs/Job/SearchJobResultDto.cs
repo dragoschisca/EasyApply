7afc425e90251d913fb87b6f6235ff2ab86c3dac
Ese Emir <tgtft1@gmail.com>
2026-05-21 22:55:24 +0300

    feat: add search job result output dto

---
+++ backend/EasyApply.BusinessLayer/Structure/DTOs/Job/SearchJobResultDto.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace EasyApply.BusinessLayer.Structure.DTOs.Job;
+
+public class SearchJobResultDto
+{
+    public List<JobDto> Jobs { get; set; } = new();
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+}
