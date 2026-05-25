19dca16649872bbee7406355c4f542db3d9815fd
Bulgaru Valeriu <valeri.bulgaru06@gmail.com>
2026-05-25 23:18:49 +0300

    feat: add Authentication validators using FluentValidation

---
+++ backend/EasyApply.BusinessLayer/Structure/Validation/AuthValidators.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using EasyApply.BusinessLayer.Structure.DTOs.Auth;
+
+namespace EasyApply.BusinessLayer.Structure.Validators;
+
+public class RegisterRequestDtoValidator : AbstractValidator<RegisterRequestDto>
+{
+    public RegisterRequestDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("A valid email is required.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one number.");
+            
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password).WithMessage("Passwords do not match.");
+
+        RuleFor(x => x.UserType)
+            .IsInEnum().WithMessage("Invalid user type.");
+    }
+}
+
+public class LoginRequestDtoValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("A valid email is required.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
