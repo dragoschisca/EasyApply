e695c04e5b4c144805f070408e7486d65a67dc0c
Bulgaru Valeriu <valeri.bulgaru06@gmail.com>
2026-05-25 23:20:26 +0300

    feat: add Company validators using FluentValidation

---
+++ backend/EasyApply.BusinessLayer/Structure/Validation/CompanyValidators.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using EasyApply.BusinessLayer.Structure.DTOs.Company;
+using System;
+
+namespace EasyApply.BusinessLayer.Structure.Validators;
+
+public class CreateCompanyDtoValidator : AbstractValidator<CreateCompanyDto>
+{
+    public CreateCompanyDtoValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(x => x.CompanyName)
+            .NotEmpty().WithMessage("Company name is required.")
+            .MaximumLength(150).WithMessage("Company name cannot exceed 150 characters.");
+
+        RuleFor(x => x.Website)
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .When(x => !string.IsNullOrEmpty(x.Website))
+            .WithMessage("Website must be a valid URL.");
+
+        RuleFor(x => x.LogoUrl)
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .When(x => !string.IsNullOrEmpty(x.LogoUrl))
+            .WithMessage("Logo URL must be a valid URL.");
+    }
+}
+
+public class UpdateCompanyDtoValidator : AbstractValidator<UpdateCompanyDto>
+{
+    public UpdateCompanyDtoValidator()
+    {
+        RuleFor(x => x.CompanyName)
+            .NotEmpty().When(x => x.CompanyName != null).WithMessage("Company name cannot be empty.")
+            .MaximumLength(150).WithMessage("Company name cannot exceed 150 characters.");
+
+        RuleFor(x => x.Website)
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .When(x => !string.IsNullOrEmpty(x.Website))
+            .WithMessage("Website must be a valid URL.");
+
+        RuleFor(x => x.LogoUrl)
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .When(x => !string.IsNullOrEmpty(x.LogoUrl))
+            .WithMessage("Logo URL must be a valid URL.");
+    }
+}
